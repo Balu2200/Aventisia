d7b30917c98e639808756efa879400ab6a7170ab
Balu Pasumarthi <balupasumarthi1@gmail.com>
2026-03-27 15:42:50 +0530

    added routes

--- routes/github.py
+++ routes/github.py
@@ -11,7 +11,7 @@ service = GithubService()
 @router.get("/repos/{username}")
 async def get_repositories(username : str):
     try:
-        return service.get_repo(username)
+        return await service.get_repo(username)
     except Exception as e:
         raise HTTPException(status_code=500, details=str(e))
 
@@ -20,10 +20,20 @@ async def get_repositories(username : str):
 @router.get("/issues/{owner}/{repo}")
 async def get_issues(owner : str,  repo: str):
     try:
-        return service.list_issues(owner, repo)
+        return await service.list_issues(owner, repo)
 
     except Exception as e:
         raise HTTPException(status_code=500, details=str(e))
 
 
 @router.post("/create-issues")
+async def create_issues(data : IssueCreate):
+    try:
+        return await service.create_issue(
+            data.owner,
+            data.repo,
+            data.title,
+            data.body
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, details=str(e))
